2b47b09f146e97ad6682cdb9255c73cfb9777660
Anibal Mendoza <anibalmd32@gmail.com>
2024-03-10 11:50:30 -0400

    feat: :sparkles: component setup

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 class PageLink extends HTMLElement {
 	constructor() {
 		super()
+		this.shadow = this.attachShadow({ mode: 'open' })
+		this.shadow.innerHTML = '<a href="#"></a>'
 	}
 
-	connectedCallback() {}
+	// * COMPONENT LIFECYCLE
+	connectedCallback() {
+		this.render()
+	}
+
 	disconnectedCallback() {}
-	static get observedAttributes() {}
+
+	// * COMPONENT METHODS
+	render() {
+		const label = this.getAttribute('label')
+		this.shadowRoot.querySelector('a').textContent = label
+	}
 }
+
+customElements.define('page-link', PageLink)
